6de65a00dc8674aabf2cdf268d656b485eba43c6
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-06 20:14:27 +0000

    fresh-next-app-typescript

---
+++ generators/fresh-next-app-typescript.js
@@ -0,0 +1,4 @@
+module.exports = {
+  command: 'yarn create next-app fresh-app && node scripts/next/setup-typescript',
+  description: 'Fresh Next app',
+}
---
+++ scripts/next/setup-typescript.js
@@ -0,0 +1,14 @@
+
+process.chdir('fresh-app')
+
+require('child_process').execSync('yarn add --dev typescript @types/react @types/node', {
+  stdio: 'inherit'
+})
+
+require('child_process').execSync('touch tsconfig.json', {
+  stdio: 'inherit'
+})
+
+require('child_process').execSync('yarn build', {
+  stdio: 'inherit'
+})
