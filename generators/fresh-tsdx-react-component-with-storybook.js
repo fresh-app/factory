bafcdad13123e2159d542ca6f5c5549078d0a01f
Thai Pangsakulyanont <dtinth@spacet.me>
2021-01-07 21:58:55 +0000

    fresh-tsdx-react-component

---
+++ generators/fresh-tsdx-react-component-with-storybook.js
@@ -0,0 +1,4 @@
+module.exports = {
+  command: 'npm config set init-author-name Author && rm -rf fresh-component && npx tsdx create fresh-component --template=react-with-storybook && mv fresh-component fresh-app',
+  description: 'Fresh React component',
+}
