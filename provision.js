de9bcf80d2a4dea1cab8097a8b36cbf0de737dd9
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-10 18:58:37 +0000

    Use prettier to format all scripts

--- provision.js
+++ provision.js
@@ -7,23 +7,29 @@ const config = require('./generators/' + process.argv[2])
 ;(async () => {
   const axiosConfig = {
     headers: {
-      'Authorization': 'Bearer ' + process.env.GH_PUSH_TOKEN,
-      'Accept': 'application/vnd.github.baptiste-preview+json'
-    }
+      Authorization: 'Bearer ' + process.env.GH_PUSH_TOKEN,
+      Accept: 'application/vnd.github.baptiste-preview+json',
+    },
   }
 
-  const { data: repo } = await axios.get('https://api.github.com/repos/fresh-app/' + project, axiosConfig).catch(async e => {
-    if (e.response?.status === 404) {
-      return axios.post('https://api.github.com/orgs/fresh-app/repos', {
-        name: project,
-        has_issues: false,
-        has_projects: false,
-        has_wiki: false,
-        is_template: true,
-      }, axiosConfig)
-    }
-    throw e
-  })
+  const { data: repo } = await axios
+    .get('https://api.github.com/repos/fresh-app/' + project, axiosConfig)
+    .catch(async (e) => {
+      if (e.response?.status === 404) {
+        return axios.post(
+          'https://api.github.com/orgs/fresh-app/repos',
+          {
+            name: project,
+            has_issues: false,
+            has_projects: false,
+            has_wiki: false,
+            is_template: true,
+          },
+          axiosConfig,
+        )
+      }
+      throw e
+    })
 
   const changes = {}
   const ensure = (k, v) => {
@@ -38,9 +44,16 @@ const config = require('./generators/' + process.argv[2])
   ensure('has_projects', false)
   ensure('has_wiki', false)
   ensure('is_template', true)
-  ensure('homepage', `https://github.com/fresh-app/factory/blob/main/generators/${project}.js`)
+  ensure(
+    'homepage',
+    `https://github.com/fresh-app/factory/blob/main/generators/${project}.js`,
+  )
   if (Object.keys(changes).length > 0) {
-    await axios.patch(`https://api.github.com/repos/fresh-app/${project}`, changes, axiosConfig)
+    await axios.patch(
+      `https://api.github.com/repos/fresh-app/${project}`,
+      changes,
+      axiosConfig,
+    )
   }
 
   console.log(repo.full_name)
