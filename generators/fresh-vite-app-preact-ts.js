d3367f0d9e8b30ef0a8d5a32be0a527f32a8ad80
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-10 18:52:27 +0000

    Add Preact vite templates

---
+++ generators/fresh-vite-app-preact-ts.js
@@ -0,0 +1,5 @@
+module.exports = {
+  command:
+    'yarn create @vitejs/app fresh-app --template=preact-ts && cd fresh-app && yarn',
+  description: 'Fresh Preact app',
+}
---
+++ generators/fresh-vite-app-preact.js
@@ -0,0 +1,5 @@
+module.exports = {
+  command:
+    'yarn create @vitejs/app fresh-app --template=preact && cd fresh-app && yarn',
+  description: 'Fresh Preact app',
+}
