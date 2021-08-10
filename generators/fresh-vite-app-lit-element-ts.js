e2df72751074bf544bdc3846e04c5756957f346c
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-10 18:54:25 +0000

    Add Lit Element

---
+++ generators/fresh-vite-app-lit-element-ts.js
@@ -0,0 +1,5 @@
+module.exports = {
+  command:
+    'yarn create @vitejs/app fresh-app --template=lit-element-ts && cd fresh-app && yarn',
+  description: 'Fresh Lit Element',
+}
---
+++ generators/fresh-vite-app-lit-element.js
@@ -0,0 +1,5 @@
+module.exports = {
+  command:
+    'yarn create @vitejs/app fresh-app --template=lit-element && cd fresh-app && yarn',
+  description: 'Fresh Lit Element',
+}
