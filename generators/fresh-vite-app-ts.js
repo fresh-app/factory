6d4eadc4fd0b1a45fe731b609bc83f0eb9680fef
Thai Pangsakulyanont <dtinth@spacet.me>
2021-08-10 18:52:07 +0000

    Import prettier config

--- generators/fresh-vite-app-ts.js
+++ generators/fresh-vite-app-ts.js
@@ -1,5 +1,5 @@
 module.exports = {
   command:
-    "yarn create @vitejs/app fresh-app --template=vanilla-ts && cd fresh-app && yarn",
-  description: "Fresh Vite app",
-};
+    'yarn create @vitejs/app fresh-app --template=vanilla-ts && cd fresh-app && yarn',
+  description: 'Fresh Vite app',
+}
