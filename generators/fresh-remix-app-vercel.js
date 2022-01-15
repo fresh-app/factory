5a10b2b7d86f8b3bc6750b6e16b23d2971c3bc2f
Thai Pangsakulyanont <dtinth@spacet.me>
2022-01-15 09:26:07 +0000

    Add fresh-remix-app-vercel template

---
+++ generators/fresh-remix-app-vercel.js
@@ -0,0 +1,5 @@
+module.exports = {
+  command:
+    'npm i -g create-remix@latest && sudo apt-get install -y expect && ./scripts/remix/quickstart-vercel && cd fresh-app && yarn',
+  description: 'Fresh Remix app',
+}
