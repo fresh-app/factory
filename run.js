c328c496e03a4bbdff24b7079d32c5b97c8165ac
Thai Pangsakulyanont <dtinth@spacet.me>
2022-04-10 07:54:22 +0000

    Dockerize

--- run.js
+++ run.js
@@ -1,30 +1,45 @@
 const config = require('./generators/' + process.argv[2])
 
-require('child_process').execSync('rm -rf tmp && mkdir -p tmp', {
-  stdio: 'inherit',
-})
+require('child_process').execSync(
+  'rm -rf workspace && docker rm -f factory-runner-instance && mkdir -p workspace/tmp',
+  {
+    stdio: 'inherit',
+  },
+)
 
-require('child_process').execSync('rm -rf fresh-app', {
-  stdio: 'inherit',
-})
+require('child_process').execFileSync(
+  'docker',
+  [
+    'run',
+    ...['--name', `factory-runner-instance`],
+    ...['-v', `${process.cwd()}:/opt/factory:ro`],
+    ...['-v', `${process.cwd()}/workspace:/workspace`],
+    ...['-i'],
+    'factory-runner',
+    ...['bash', '-ex'],
+  ],
+  {
+    stdio: ['pipe', 'inherit', 'inherit'],
+    input: config.command,
+  },
+)
 
-require('child_process').execSync(config.command, {
-  stdio: 'inherit',
-})
-
-require('fs').writeFileSync('tmp/project', process.argv[2])
+require('fs').writeFileSync('workspace/tmp/project', process.argv[2])
 
 const size = parseInt(
-  require('child_process').execSync('du --bytes --summarize fresh-app', {
-    encoding: 'utf8',
-  }),
+  require('child_process').execSync(
+    'du --bytes --summarize workspace/fresh-app',
+    {
+      encoding: 'utf8',
+    },
+  ),
   10,
 )
 
 const sizeText = (size / 1e6).toFixed(1) + ' MB'
 
 require('fs').writeFileSync(
-  'tmp/message',
+  'workspace/tmp/message',
   config.description +
     ' as of ' +
     new Date(Date.now() - 86400e3).toISOString().split('T')[0] +
--- sync.sh
+++ sync.sh
@@ -4,11 +4,11 @@ export GIT_AUTHOR_NAME=dtinth-bot
 export GIT_COMMITTER_EMAIL=dtinth-bot@users.noreply.github.com
 export GIT_AUTHOR_EMAIL=dtinth-bot@users.noreply.github.com
 
-cd fresh-app
+cd workspace/fresh-app
 rm -rf .git
 git init
 git remote add origin "https://x-access-token:$GH_PUSH_TOKEN@github.com/fresh-app/$(cat ../tmp/project).git"
 git fetch origin
 git reset --soft origin/main || echo "No existing branch"
 git add --all && git commit -m "$(cat ../tmp/message)" || echo "Nothing to commit"
-git push origin HEAD:refs/heads/main
+# git push origin HEAD:refs/heads/main
