bf31c313f9e01a008e7aa53368876cf3dcf25847
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-02 20:36:58 +0000

    Add playwright test screenshots

--- src/generators/fresh-playwright-project.ts
+++ src/generators/fresh-playwright-project.ts
@@ -1,11 +1,15 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command:
+  command: [
     'yarn create playwright fresh-app --quiet --lang=TypeScript --gha --no-browsers',
+    'cd fresh-app',
+    'yarn playwright test',
+  ].join('\n'),
   displayedCommand: 'yarn create playwright',
   description: 'Fresh Playwright Test project',
   longDescription: 'Fresh Playwright Test project with TypeScript',
   frameworkUrl: 'https://playwright.dev/',
   frameworkDocumentationUrl: 'https://playwright.dev/docs/intro',
+  staticOutputDirectory: 'playwright-report',
 })
