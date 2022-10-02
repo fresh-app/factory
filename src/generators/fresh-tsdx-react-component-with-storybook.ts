f3ebbb3740185c04805393f26ef7f6a42950968f
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-02 20:21:05 +0000

    Add tsdx storybook screenshots

--- src/generators/fresh-tsdx-react-component-with-storybook.ts
+++ src/generators/fresh-tsdx-react-component-with-storybook.ts
@@ -1,8 +1,15 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command:
-    'npm config set init-author-name Author && rm -rf fresh-component && npx tsdx create fresh-component --template=react-with-storybook && mv fresh-component fresh-app',
+  command: [
+    'npm config set init-author-name Author',
+    'npx tsdx create fresh-component --template=react-with-storybook',
+    'mv fresh-component fresh-app',
+    'cd fresh-app',
+    'grep -q storybook-static .gitignore || echo storybook-static >> .gitignore',
+    'yarn build',
+    'yarn build-storybook',
+  ].join('\n'),
   description: 'Fresh React component',
   repoDescriptionOverride:
     'Fresh React component library built with TSDX, storybooks included, automatically generated everyday from its official generator script and template',
