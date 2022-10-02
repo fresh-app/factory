68e696af1a8aeeb1c0efe063a8cce6f2bc560adf
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-02 18:01:21 +0000

    Fix redwood command

--- src/generators/fresh-redwood-app-typescript.ts
+++ src/generators/fresh-redwood-app-typescript.ts
@@ -1,12 +1,7 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command: [
-    'yarn create redwood-app fresh-app --typescript --no-yarn-install',
-    'cd fresh-app',
-    'yarn',
-    'yarn rw-gen',
-  ].join('\n'),
+  command: 'yarn create redwood-app fresh-app --typescript',
   description: 'Fresh Redwood app',
   frameworkUrl: 'https://redwoodjs.com/',
   frameworkDocumentationUrl: 'https://redwoodjs.com/docs/tutorial/foreword',
