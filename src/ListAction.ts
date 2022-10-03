0b7ad85de72052f3293a09d204d8cda8d4419078
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-03 06:27:58 +0000

    Add list --json

--- src/ListAction.ts
+++ src/ListAction.ts
@@ -1,10 +1,14 @@
-import { CommandLineAction } from '@rushstack/ts-command-line'
+import {
+  CommandLineAction,
+  CommandLineFlagParameter,
+} from '@rushstack/ts-command-line'
 import { Generator } from './defineGenerator'
 import glob from 'glob'
 import { basename } from 'path'
 import { getRepoDescription } from './getRepoDescription'
 
 export class ListAction extends CommandLineAction {
+  private _json!: CommandLineFlagParameter
   public constructor() {
     super({
       actionName: 'list',
@@ -12,16 +16,37 @@ export class ListAction extends CommandLineAction {
       documentation: 'List all the generators',
     })
   }
-  protected onDefineParameters(): void {}
+  protected onDefineParameters(): void {
+    this._json = this.defineFlagParameter({
+      parameterLongName: '--json',
+      description: 'Output in JSON format',
+    })
+  }
   protected async onExecute(): Promise<void> {
     const files = glob.sync('src/generators/*.ts')
-    for (const file of files.sort()) {
-      const name = basename(file, '.ts')
-      console.log(`${name.padEnd(48)} ${file}`)
-      const { default: generator } = (await import(
-        `./generators/${name}.ts`
-      )) as { default: Generator }
-      console.log('    ' + getRepoDescription(generator))
+    if (this._json.value) {
+      const generators: any[] = []
+      for (const file of files.sort()) {
+        const name = basename(file, '.ts')
+        console.log(`${name.padEnd(48)} ${file}`)
+        const generator = await this.loadGenerator(name)
+        const description = getRepoDescription(generator)
+        generators.push({ name, description, info: generator })
+      }
+      console.log(JSON.stringify(generators, null, 2))
+    } else {
+      for (const file of files.sort()) {
+        const name = basename(file, '.ts')
+        console.log(`${name.padEnd(48)} ${file}`)
+        const generator = await this.loadGenerator(name)
+        console.log('    ' + getRepoDescription(generator))
+      }
     }
+  }
+  private async loadGenerator(name: string) {
+    const { default: generator } = (await import(
+      `./generators/${name}.ts`
+    )) as { default: Generator }
+    return generator
   }
 }
