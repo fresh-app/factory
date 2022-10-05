3f7ef883f706c301866edf11f608fe749a9ecc77
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-05 10:46:22 +0700

    Fix formatting and metadata

--- src/generators/fresh-docusaurus-site.ts
+++ src/generators/fresh-docusaurus-site.ts
@@ -1,15 +1,14 @@
-import { defineGenerator } from "../defineGenerator";
+import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-    command: [
-        'yarn create docusaurus fresh-app classic --typescript',
-        'cd fresh-app',
-        'yarn build',
-    ].join('\n'),
-    displayedCommand: 'yarn create docusaurus classic --typescript',
-    description: '',
-    frameworkUrl: 'https://docusaurus.io',
-    frameworkDocumentationUrl: 'https://docusaurus.io/docs',
-    serverCommand: 'yarn start',
-    serverPort: 3000
+  command: [
+    'yarn create docusaurus fresh-app classic --typescript',
+    'cd fresh-app',
+    'yarn build',
+  ].join('\n'),
+  displayedCommand: 'yarn create docusaurus classic --typescript',
+  description: 'Fresh Docusaurus site',
+  frameworkUrl: 'https://docusaurus.io',
+  frameworkDocumentationUrl: 'https://docusaurus.io/docs',
+  staticOutputDirectory: 'build',
 })
