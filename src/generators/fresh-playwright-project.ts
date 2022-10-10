8dd49b7e6ca293109d9d339c6321b19d741555e0
Thai Pangsakulyanont <dtinth@spacet.me>
2022-10-10 06:37:52 +0000

    Fix playwright factory

--- src/generators/fresh-playwright-project.ts
+++ src/generators/fresh-playwright-project.ts
@@ -2,8 +2,11 @@ import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
   command: [
-    'yarn create playwright fresh-app --quiet --lang=TypeScript --gha',
+    'yarn create playwright fresh-app --quiet --lang=TypeScript --gha --no-browsers',
     'cd fresh-app',
+    'export PLAYWRIGHT_BROWSERS_PATH=/workspace/pw-browsers  # Do not interfere with browsers provided by Docker image',
+    'sudo rm -f /ms-playwright/.docker-info                  # Trick Playwright that we are not in Docker',
+    'sudo -E yarn playwright install --with-deps             # Install browsers because it may be out-of-sync with Playwright image',
     'yarn playwright test',
   ].join('\n'),
   displayedCommand: 'yarn create playwright',
