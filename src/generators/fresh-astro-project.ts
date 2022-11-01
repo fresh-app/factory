3089a85cf9013582c63d569bbd8bddeaba49da7e
Thai Pangsakulyanont <dtinth@spacet.me>
2022-11-01 10:43:43 +0000

    Fix astro template

--- src/generators/fresh-astro-project.ts
+++ src/generators/fresh-astro-project.ts
@@ -1,11 +1,19 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command: [
-    'yarn create astro fresh-app --template basics --install --git=false --typescript=strict',
-    'cd fresh-app',
-    'yarn build',
-  ].join('\n'),
+  script: async (t) => {
+    await t.send(
+      'yarn create astro fresh-app --template basics --install --git=false --typescript=strict',
+    )
+    await t.waitForText('project')
+    await t.waitForText('Done')
+    await t.waitForText('$')
+    await t.send('cd fresh-app')
+    await t.waitForText('$')
+    await t.send('yarn build')
+    await t.waitForText('Done')
+    await t.waitForText('$')
+  },
   displayedCommand: 'yarn create astro --template basics',
   description: 'Fresh Astro project',
   longDescription: 'Fresh Astro project, with TypeScript',
