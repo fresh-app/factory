b7a97ab7276929a38df66e5cd10792d0d3952866
Thai Pangsakulyanont <dtinth@spacet.me>
2022-12-28 09:57:53 +0000

    Fix redwood factory

--- src/generators/fresh-redwood-app-typescript.ts
+++ src/generators/fresh-redwood-app-typescript.ts
@@ -1,8 +1,8 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command: 'yarn create redwood-app fresh-app --typescript',
-  displayedCommand: 'yarn create redwood-app --typescript',
+  command: 'yarn create redwood-app fresh-app --typescript --git-init=false',
+  displayedCommand: 'yarn create redwood-app --typescript --git-init=false',
   description: 'Fresh Redwood app',
   longDescription: 'Fresh Redwood app, with TypeScript',
   frameworkUrl: 'https://redwoodjs.com/',
--- src/generators/fresh-redwood-app.ts
+++ src/generators/fresh-redwood-app.ts
@@ -1,8 +1,10 @@
 import { defineGenerator } from '../defineGenerator'
 
 export default defineGenerator({
-  command: 'yarn create redwood-app fresh-app --typescript=false',
-  displayedCommand: 'yarn create redwood-app --typescript=false',
+  command:
+    'yarn create redwood-app fresh-app --typescript=false --git-init=false',
+  displayedCommand:
+    'yarn create redwood-app --typescript=false --git-init=false',
   description: 'Fresh Redwood app',
   frameworkUrl: 'https://redwoodjs.com/',
   frameworkDocumentationUrl: 'https://redwoodjs.com/docs/tutorial/foreword',
