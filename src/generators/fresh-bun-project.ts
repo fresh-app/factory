87d615fb426530bdedae8b257a286f6734160a96
Thai Pangsakulyanont <dtinth@spacet.me>
2023-01-29 09:26:23 +0000

    Add bun project template

---
+++ src/generators/fresh-bun-project.ts
@@ -0,0 +1,13 @@
+import { defineGenerator } from '../defineGenerator'
+
+export default defineGenerator({
+  command: [
+    'curl -fsSL https://bun.sh/install | bash',
+    '(mkdir -p fresh-app && cd fresh-app && ~/.bun/bin/bun init -y)'
+  ].join('\n'),
+  displayedCommand: 'bun init',
+  description: 'Fresh Bun project',
+  longDescription: 'Fresh Bun project, with TypeScript',
+  frameworkUrl: 'https://bun.sh/',
+  frameworkDocumentationUrl: 'https://github.com/oven-sh/bun#readme',
+})
