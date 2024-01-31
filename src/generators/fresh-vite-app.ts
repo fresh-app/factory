898499ee4b4a12e458eb825a5f0090960c7310f7
Thai Pangsakulyanont <dtinth@spacet.me>
2024-01-31 14:37:55 +0000

    separate runner and screenshotter

--- src/generators/fresh-vite-app.ts
+++ src/generators/fresh-vite-app.ts
@@ -3,8 +3,8 @@ import { defineGenerator } from '../defineGenerator'
 export default defineGenerator({
   description: 'Fresh Vite app',
   command:
-    'yarn create vite fresh-app --template=vanilla && cd fresh-app && yarn && yarn build',
-  displayedCommand: 'yarn create vite fresh-app --template=vanilla',
+    'pnpm create vite fresh-app --template=vanilla && cd fresh-app && pnpm install && pnpm build',
+  displayedCommand: 'pnpm create vite fresh-app --template=vanilla',
   frameworkUrl: 'https://vitejs.dev/',
   frameworkDocumentationUrl: 'https://vitejs.dev/guide/',
   staticOutputDirectory: 'dist',
